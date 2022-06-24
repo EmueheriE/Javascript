37cc8ea97125a2903fb1d494362571286869a3b8
Lovetemi <105287941+EmueheriE@users.noreply.github.com>
2022-06-24 16:16:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("Welcome to my first javascript code")
+
+alert("My name is Emueheri Loveth Eguono")
+
+alert("I am 5.2 inchies tall")
+
+alert("I am from Nigeria")
+
+let name = "My name is Emueheri Loveth Eguono"
+let height = "I am 5.2 inchies tall"
+let country = "I am from Nigeria"
+
+console.log(name);
+console.log(height);
+console.log(country);
